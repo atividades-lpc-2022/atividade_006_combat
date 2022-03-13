a45afbe94b71326ae7003e14ef79e1f94a9bc28f
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2022-03-13 16:53:32 -0400

    add tank and ball mechanics on colliding

--- src/modules/Ball.py
+++ src/modules/Ball.py
@@ -1,4 +1,5 @@
 import pygame
+from modules.Brick import Brick
 from modules.Coordinate import Coordinate
 from modules.Dimension import Dimension
 from modules.Screen import Screen
@@ -9,9 +10,33 @@ class Ball:
         self.coordinate = coordinate
         self.image = pygame.Surface((5, 5))
         self.rect = self.image.get_rect()
+        self.dimension = Dimension(self.rect.width, self.rect.height)
         self.image.fill(color)
         self.hits = 0  # number of hits on the wall
-        self.velocity = 1.0  # ball velocity
+        self.x_velocity = 1.0  # ball velocity
+        self.y_velocity = 1.0  # ball velocity
+
+    def change_direction(self, brick: Brick): # TODO: Add ball collision with the bricks to undo this comment
+        if self.coordinate.x == brick.coordinate.x:
+            percents = (self.coordinate.y - brick.coordinate.y)/ brick.dimension.height
+
+            if percents > 0.5:
+                self.x_velocity *= -1
+                self.y_velocity = 1
+            elif percents < 0.5:
+                self.x_velocity *= -1
+                self.y_velocity = -1
+
+        elif self.coordinate.y == brick.coordinate.y:
+            percents = (self.coordinate.x - brick.coordinate.x)/ brick.dimension.width
+
+            if percents > 0.5:
+                self.x_velocity = -1
+                self.y_velocity *= -1
+            elif percents < 0.5:
+                self.x_velocity = 1
+                self.y_velocity *= -1
+
 
     def fires_at(
         self, screen: Screen
--- src/modules/Tank.py
+++ src/modules/Tank.py
@@ -1,7 +1,24 @@
+from random import randint
+from secrets import choice
+from typing import Sequence
 import pygame
+from config import Config
 from modules.Coordinate import Coordinate
+from modules.Dimension import Dimension
 from modules.Screen import Screen
 
+BRICK_POSITIONS = [
+    [400, 180, 35, 80],
+    [400, 475, 35, 80],
+    [230, 325, 80, 30],
+    [570, 325, 80, 30],
+    [120, 325, 7, 110],
+    [103, 278, 17, 17],
+    [103, 371, 17, 17],
+    [680, 325, 7, 110],
+    [697, 278, 17, 17],
+    [697, 371, 17, 17]   
+]
 
 class Tank(pygame.sprite.Sprite):
     def __init__(self, coordinate: Coordinate, sprite_path: str):
@@ -10,7 +27,9 @@ class Tank(pygame.sprite.Sprite):
         self.image = pygame.image.load(sprite_path)
         self.rect = self.image.get_rect()
         self.coordinate = coordinate
-
+        self.dimension = Dimension(self.rect.width, self.rect.height)
+        self.range_allowed_x = list(range(21, Config.SCREEN_WIDTH - self.dimension.width + 1))
+        self.range_allowed_y = list(range(76, Config.SCREEN_HEIGHT - self.dimension.height + 1))
         self.velocity = 1.0  # Default tank velocity
         self.angle = 0  # Default tank angle
 
@@ -18,6 +37,17 @@ class Tank(pygame.sprite.Sprite):
         self.image = pygame.transform.rotate(self.image, new_angle)
         rot_rect = self.image.get_rect(center=(self.rect.center))
         
+    def change_position(self): # TODO: Add tank collision with the ball to undo this comment
+        new_x = choice(self.range_allowed_x)
+        new_y = choice(self.range_allowed_y)
+
+        for brick_position in BRICK_POSITIONS:
+            brick_x, brick_y, brick_width, brick_height = brick_position
+            surface_colliding = (brick_x - self.dimension.width <= new_x <= brick_x + brick_width) and (brick_y - self.dimension.height <= new_y <= brick_y + brick_height)
+            if surface_colliding: return self.change_position()
+
+        self.coordinate.x = new_x
+        self.coordinate.y = new_y
 
     def use_controls(self):  # TODO: Define tank controls
         print("Setting tank controls...")
